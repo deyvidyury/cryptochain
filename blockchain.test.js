a7ddfdb1f28c849b514dc68ad0086119f8989837
Deyvid Lopes <deyvidyury@yahoo.com.br>
2019-02-05 16:42:41 -0300

    Blockchain class

--- blockchain.test.js
+++ blockchain.test.js
@@ -1,21 +1,21 @@
-const Blockchain = require('./blockchain')
-const Block = require('./block')
+const Blockchain = require("./blockchain");
+const Block = require("./block");
 
-describe('Blockchain()', () => {
-    const blockchain = new Blockchain()
+describe("Blockchain()", () => {
+  const blockchain = new Blockchain();
 
-    it('contains a chain array instance', () => {
-        expect(blockchain.chain instanceof Array).toBe(true);
-    })
+  it("contains a chain array instance", () => {
+    expect(blockchain.chain instanceof Array).toBe(true);
+  });
 
-    it('starts with the genesis block', () => {
-        expect(blockchain.chain[0]).toEqual(Block.genesis())
-    })
+  it("starts with the genesis block", () => {
+    expect(blockchain.chain[0]).toEqual(Block.genesis());
+  });
 
-    it('adds a new block to the chain', () => {
-        const newData = 'foo bar';
-        blockchain.addBlock({data: newData})
+  it("adds a new block to the chain", () => {
+    const newData = "foo bar";
+    blockchain.addBlock({ data: newData });
 
-        expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData)
-    })
-})
+    expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+  });
+});
