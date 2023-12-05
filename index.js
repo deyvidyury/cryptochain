8eb998d47c2f801ca9493aaa8f2e3120f19fe10a
deyvidyury <deyvidyury@yahoo.com.br>
2023-12-05 15:46:20 -0300

    sync chain at start up

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express')
 const Blockchain = require('./blockchain');
 const bodyParser = require('body-parser');
 const PubSub = require('./pubsub');
+const request = require('request');
 
 const app = express();
 const blockchain = new Blockchain();
 const pubSub = new PubSub({blockchain});
 
+const DEFAULT_PORT = 3000
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
+
 setTimeout(()=> 
-pubSub.broadcastChain()
+  pubSub.broadcastChain()
 , 1000);
 
 app.use(bodyParser.json())
@@ -27,7 +31,17 @@ app.post('/api/mine',(req, res) => {
   res.redirect('/api/blocks')
 })
 
-const DEFAULT_PORT = 3000
+const syncChains = () => {
+  request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const rootChain = JSON.parse(body)
+      console.log('replacing chain on a sync with ', rootChain)
+      blockchain.replaceChain(rootChain);
+    }
+  })
+}
+
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
@@ -37,4 +51,5 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
 const PORT = PEER_PORT || DEFAULT_PORT
 app.listen(PORT, ()=>{
   console.log(`Server running on port ${PORT}`)
+  syncChains();
 }) 
